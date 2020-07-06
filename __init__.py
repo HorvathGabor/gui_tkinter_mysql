2cc7430baed0bd373543e7277da992295e366743
Gabor Horvath <hage13@gmail.com>
2020-07-06 16:11:31 +0200

    Main window

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 14:54:41 2020
+
+@author: gaborhorvath
+"""
---
+++ main_gui.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 16:09:53 2020
+
+@author: gaborhorvath
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on: 2020-07-06
+
+@author: gaborhorvath
+"""
